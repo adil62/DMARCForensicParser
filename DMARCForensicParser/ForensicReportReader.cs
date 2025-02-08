922e51f0f4ba76e9419cc531caecf8d689fe33d2
Adil Ismail <adilismail62@gmail.com>
2025-02-08 19:29:42 +0530

    init

---
+++ DMARCForensicParser/ForensicReportReader.cs
@@ -0,0 +1,13 @@
+﻿using MimeKit;
+
+namespace DMARCForensicParser {
+    public class ForensicReportReader
+    {
+        public void ReadForensicReport(Stream stream)
+        {
+            var message = MimeMessage.Load(stream);
+
+            
+        }
+    }
+}
